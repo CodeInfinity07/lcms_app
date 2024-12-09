d6a926edcf041db747d63d04b5149d3c12127fbb
Zain Riaz <ryzon@kali.xerik>
2024-12-09 18:32:13 +0500

    Updates

--- resources/views/admin/layouts/head-css.blade.php
+++ resources/views/admin/layouts/head-css.blade.php
@@ -1,11 +1,11 @@
 @yield('css')
 
 <!-- Bootstrap Css -->
-<link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-<link href="{{ URL::asset('assets/css/bracket.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('assets/css/bootstrap.min.css?v=1.0') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('assets/css/bracket.css?v=1.0') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="{{ URL::asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('assets/css/icons.min.css?v=1.0') }}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="{{ URL::asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('assets/css/app.min.css?v=1.0') }}" id="app-style" rel="stylesheet" type="text/css" />
 <!-- App js -->
-<script src="{{ URL::asset('assets/js/plugin.js') }}"></script>
+<script src="{{ URL::asset('assets/js/plugin.js?v=1.0') }}"></script>
